8e6da9efbf3df38cf9b51c9fdca84e0ca18543fc
errwiinn <97678557+errwiinn@users.noreply.github.com>
2022-03-01 21:20:07 +0700

    Practice 2

---
+++ Practice 2/ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -0,0 +1,16 @@
+package problemset2_2;
+
+public class ProblemSet2_2 {
+
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        System.out.println("  /\\ /\\  ");
+        System.out.println(" / ^   ^ \\ ");
+        System.out.println("\\    3   /");
+        System.out.println("\\-------/");
+        System.out.println(" ---------");
+        //Aku sangat menyukai kucingku^-^
+
+    }
+}
