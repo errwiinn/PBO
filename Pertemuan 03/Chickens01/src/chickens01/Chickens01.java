7d548238784211fae7f0f5fda3928cfa051c9952
errwiinn <97678557+errwiinn@users.noreply.github.com>
2022-03-01 21:38:16 +0700

    Pertemuan 03

---
+++ Pertemuan 03/Chickens01/src/chickens01/Chickens01.java
@@ -0,0 +1,29 @@
+package chickens01;
+
+public class Chickens01 {
+
+    public static void main(String[] args) {
+        //Put yout code here
+        int eggsPerChicken, chickenCount;
+        int totalEggs;
+        eggsPerChicken = 5;
+        chickenCount = 3;
+        totalEggs = eggsPerChicken * chickenCount;
+        chickenCount++;
+        totalEggs = totalEggs + (eggsPerChicken * chickenCount);
+        chickenCount = chickenCount / 2;
+        totalEggs = totalEggs + (eggsPerChicken * chickenCount);
+        System.out.println(totalEggs);
+
+
+        totalEggs = 0;
+        eggsPerChicken = 4;
+        chickenCount = 8;
+        totalEggs = eggsPerChicken * chickenCount;
+        chickenCount++;
+        totalEggs = totalEggs + (eggsPerChicken * chickenCount);
+        chickenCount = chickenCount / 2;
+        totalEggs = totalEggs + (eggsPerChicken * chickenCount);
+        System.out.println(totalEggs);
+    }
+}
